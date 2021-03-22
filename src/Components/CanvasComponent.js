7368a271c1c2fc3b27de5a7a409f807dd77df300
LeoRoma <xiajtn@gmail.com>
2021-03-22 17:00:21 +0000

    Created a circl

--- src/Components/CanvasComponent.js
+++ src/Components/CanvasComponent.js
@@ -9,14 +9,26 @@ class CanvasComponent extends Component {
     componentDidMount() {
         const canvas = this.canvasRef.current;
         canvas.width = 800;
-        canvas.height= 450;
+        canvas.height = 450;
+
         const context = canvas.getContext('2d');
+        // To give color to a rectangle
+        context.fillStyle = "blue";
+        //  creates a rectangle
         context.fillRect(100, 100, 100, 100);
+        //to draw lines
         context.beginPath();
         context.moveTo(50, 300);
         context.lineTo(300, 100);
-        context.lineTo(400, 300)
+        context.lineTo(400, 300);
+        // to give color to lines
         context.strokeStyle = "red";
+        context.stroke();
+
+        // Arc or Circle
+        context.beginPath();
+        context.arc(300, 300, 50, 0, Math.PI, true);
+        context.strokeStyle = "green"
         context.stroke();
     }
 
