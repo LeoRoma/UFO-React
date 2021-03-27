ff29336f91a3fa8ad075be712a18da1f43b141a3
LeoRoma <xiajtn@gmail.com>
2021-03-27 17:05:56 +0000

    Responsive canvas check 2

--- src/Components/Canvas.js
+++ src/Components/Canvas.js
@@ -19,14 +19,16 @@ import UFO from './UFO';
 function Canvas(animationsInfo) {
     const canvasRef = useRef(null);
     const { ufoYAxis, width, height, shootingStarX, shootingStarY, tankX } = animationsInfo;
-    const scale = window.innerWidth / width
 
     useEffect(() => {
         const canvas = canvasRef.current;
         const ctx = canvas.getContext("2d");
         canvas.width = width;
         canvas.height = height;
-     
+        if(window.innerWidth <= 800){
+            canvas.width = window.innerWidth;
+            canvas.height = window.innerHeight;
+        }
 
         ctx.clearRect(0, 0, width, height);
 
@@ -49,7 +51,7 @@ function Canvas(animationsInfo) {
 
     })
 
-    return <canvas id="canvas" ref={canvasRef} scaleX={scale} />
+    return <canvas id="canvas" ref={canvasRef} />
 }
 
 export default Canvas
