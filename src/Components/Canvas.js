9d1126d24ba9a2b5c11454f1c57a40bc6564e2df
LeoRoma <xiajtn@gmail.com>
2021-03-27 16:12:26 +0000

    Refactored TheEye

--- src/Components/Canvas.js
+++ src/Components/Canvas.js
@@ -1,27 +1,25 @@
 import React, { useRef, useEffect} from 'react';
 
+import TheEye2 from './TheEye2';
+
 import BigBen from './BigBen';
 import CallBox from './CallBox';
 import Clouds from './Clouds';
 import Moon from './Moon';
+import ShootingStar from './ShootingStar';
 import Sky from './Sky';
 import Stars from './Stars';
-
-import TheEye from './TheEye';
 import Street from './Street';
+import Tank from './Tank';
+import TheEye from './TheEye';
 import TowerBridge from './TowerBridge';
 import UFO from './UFO';
-import ShootingStar from './ShootingStar';
-import Tank from './Tank';
-
 
 
 function Canvas(animationsInfo) {
     const canvasRef = useRef(null);
     const {ufoYAxis, width, height, shootingStarX, shootingStarY, tankX} = animationsInfo;
 
- 
-    // console.log(stars);
     useEffect(() => {
         const canvas = canvasRef.current;
         const ctx = canvas.getContext("2d");
@@ -30,12 +28,11 @@ function Canvas(animationsInfo) {
 
         ctx.clearRect(0, 0, width, height);
         
-       
-
         Sky(ctx);
         Stars(ctx);
         Moon(ctx);
         Clouds(ctx);
+        TheEye2(ctx);
         TheEye(ctx);
         BigBen(ctx);
         TowerBridge(ctx);
---
+++ src/Components/TheEye2.js
@@ -0,0 +1,52 @@
+function TheEye2(ctx) {
+    const centreX = 400;
+    const centreY = 200;
+    const radius = 150;
+    let rotateAngle = 36 * Math.PI / 180;
+    let startAngle = 0 * Math.PI / 180;
+    let endAngle = 36 * Math.PI / 180;
+
+    function drawWheel() {
+        ctx.beginPath(); // cabins from top to right
+        ctx.arc(400, 200, 130, 0, Math.PI * 2);
+        ctx.fillStyle = "black";
+        ctx.stroke();
+        ctx.closePath();
+
+        for (let i = 0; i < 10; i++) {
+            // ctx.fillStyle = gradient;  
+            ctx.translate(centreX, centreY);
+            ctx.rotate(rotateAngle);
+            ctx.translate(-centreX, -centreY);
+            ctx.beginPath();
+            ctx.moveTo(centreX, centreY);
+            ctx.lineTo(centreX + radius, centreY);
+            ctx.arc(centreX, centreY, radius, startAngle, endAngle, false);
+            ctx.closePath();
+            ctx.stroke();
+        }
+
+    }
+
+    function createCabin(ctx, x, y) {
+        ctx.beginPath(); // cabins from top to right
+        ctx.arc(x, y, 10, 0, Math.PI * 2);
+        ctx.fillStyle = "black";
+        ctx.fill();
+        ctx.closePath();
+    }
+    createCabin(ctx, 351, 48); // top left
+    createCabin(ctx, 450, 48); // top right
+    createCabin(ctx, 529, 106);
+    createCabin(ctx, 560, 200);// mid right
+    createCabin(ctx, 529, 295);
+    createCabin(ctx, 450, 352); // bottom right
+    createCabin(ctx, 351, 352);
+    createCabin(ctx, 271, 295);
+    createCabin(ctx, 240, 200);
+    createCabin(ctx, 271, 106);
+
+    drawWheel(ctx);
+}
+
+export default TheEye2;
