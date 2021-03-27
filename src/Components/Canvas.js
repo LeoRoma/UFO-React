332fdc4aec3828f7f5c9278fc3839e2250f66d8d
LeoRoma <xiajtn@gmail.com>
2021-03-27 16:51:26 +0000

    Responsive canvas check 1

--- src/Components/Canvas.js
+++ src/Components/Canvas.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect} from 'react';
+import React, { useRef, useEffect } from 'react';
 
 import TheEye2 from './TheEye2';
 
@@ -18,16 +18,18 @@ import UFO from './UFO';
 
 function Canvas(animationsInfo) {
     const canvasRef = useRef(null);
-    const {ufoYAxis, width, height, shootingStarX, shootingStarY, tankX} = animationsInfo;
+    const { ufoYAxis, width, height, shootingStarX, shootingStarY, tankX } = animationsInfo;
+    const scale = window.innerWidth / width
 
     useEffect(() => {
         const canvas = canvasRef.current;
         const ctx = canvas.getContext("2d");
         canvas.width = width;
         canvas.height = height;
+     
 
         ctx.clearRect(0, 0, width, height);
-        
+
         Sky(ctx);
         Stars(ctx);
         Moon(ctx);
@@ -36,16 +38,18 @@ function Canvas(animationsInfo) {
         TheEye(ctx);
         BigBen(ctx);
         TowerBridge(ctx);
-      
+
         Street(ctx, width);
         CallBox(ctx);
         UFO(ctx, ufoYAxis);
         ShootingStar(ctx, shootingStarX, shootingStarY);
         Tank(ctx, tankX)
         ctx.restore();
+
+
     })
 
-    return <canvas id="canvas" ref={canvasRef} />
+    return <canvas id="canvas" ref={canvasRef} scaleX={scale} />
 }
 
 export default Canvas
